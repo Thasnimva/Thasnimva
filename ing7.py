d0a07c0054ef497365b744bc00914397132222ae
Thasnimva <93870848+Thasnimva@users.noreply.github.com>
2021-12-01 21:55:04 +0530

    Add files via upload

---
+++ ing7.py
@@ -0,0 +1,9 @@
+str=input("enter a string")
+print("inputted string is ",str)
+if(str.endswith("ing")):
+    str=str+'ly'
+else:
+ str=str+'ing'
+print("the formatted string is",str)
+
+    
---
+++ longest8.py
@@ -0,0 +1,13 @@
+a=[]
+n=int(input("enter the  number of elements in list"))
+for x in range(0,n):
+  element=input("enter element"+str(x+1))
+  a.append(element)
+  max1=len(a[0])
+  temp=a[0]
+for i in a:
+    if(len(i)>max1):
+        max1=len(i)
+        temp=i
+print("longest word",temp)
+print("length of longets word:",max1)
